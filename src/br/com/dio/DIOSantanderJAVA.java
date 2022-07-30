e5410a3f12eb35c1b11dedaf91e4f086169dd839
AlexWilliam <alexgoncalves.ti@gmail.com>
2022-07-30 10:59:09 -0300

    Adicionando novos arquivos

--- src/br/com/dio/DIOSantanderJAVA.java
+++ src/br/com/dio/DIOSantanderJAVA.java
@@ -2,11 +2,17 @@ package br.com.dio;
 
 import br.com.dio.model.Gato;
 
+import java.util.Objects;
+
 public class DIOSantanderJAVA {
 
     public static void main(String[] args) {
 
         Gato gato = new Gato();
+
+        Livro livro = new Livro("Contos Inacabados", 400);
+
+        System.out.println(livro);
 
         System.out.println(gato);
 
@@ -15,5 +21,54 @@ public class DIOSantanderJAVA {
         System.out.println("Hello World!" + (a+b));*/
 
 
+    }
+}
+
+class Livro {
+    private String nome;
+    private Integer numPaginas;
+
+    public Livro() {}
+
+    public Livro(String nome, Integer numPaginas) {
+        this.nome = nome;
+        this.numPaginas = numPaginas;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getNumPaginas() {
+        return numPaginas;
+    }
+
+    public void setNumPaginas(Integer numPaginas) {
+        this.numPaginas = numPaginas;
+    }
+
+    @Override
+    public String toString() {
+        return "Livro{" +
+                "nome='" + nome + '\'' +
+                ", numPaginas=" + numPaginas +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Livro livro = (Livro) o;
+        return Objects.equals(nome, livro.nome) && Objects.equals(numPaginas, livro.numPaginas);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nome, numPaginas);
     }
 }
